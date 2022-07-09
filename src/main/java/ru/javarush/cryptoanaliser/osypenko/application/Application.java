4c1cafc2e287eacf0e9d26ba77290c1547ce6fc3
Oleksandr <108687617+OleksandrOsypenko89@users.noreply.github.com>
2022-07-09 12:28:16 +0300

    Revert "Revert "Encoder works.""
    This reverts commit f97f558fbef0f028b308fecda9280733f8e379be.

--- src/main/java/ru/javarush/cryptoanaliser/osypenko/application/Application.java
+++ src/main/java/ru/javarush/cryptoanaliser/osypenko/application/Application.java
@@ -1,14 +1,13 @@
 package ru.javarush.cryptoanaliser.osypenko.application;
 
-import ru.javarush.cryptoanaliser.osypenko.question.StartQuestion;
+import ru.javarush.cryptoanaliser.osypenko.controller.Controller;
 import java.util.Scanner;
 
 public class Application {
-
+    public static void run() {
+        Controller.question();
+    }
     public static Scanner scan() {
         return new Scanner(System.in);
-    }
-    public static void startApplication() {
-        StartQuestion.question();
     }
 }
